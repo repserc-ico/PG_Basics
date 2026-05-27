44b6961b2b3ea5638e20f9519fbb78a1a560bf14
repserc-ico <kameshima@std.it-college.ac.jp>
2026-05-27 07:52:20 +0000

    例題13から26まで

---
+++ Chapter03/ex3-16.py
@@ -0,0 +1,5 @@
+print(1 == 1 and 2 == 2) #両辺ともTrue
+print(1 == 2 and 2 == 2) #左辺がFalse
+print(1 == 2 and 2 == 1) #両辺ともFalse
+print(2 == 1 and 1 == 1) #右辺がFalse
+print(1 == 1 and 10 != 2 and 2 < 10) #いずれもTrue
---
+++ Chapter03/ex3-17.py
@@ -0,0 +1,5 @@
+print(1 == 1 or 1 == 2) #右辺がFalse
+print(1 == 1 or 2 == 2) #両辺ともTrue
+print(1 == 2 or 2 == 1) #両辺ともFalse
+print(2 == 1 or 1 == 2) #両辺ともFalse
+print(1 == 1 or 1 == 2 or 1 == 3) #一番左だけがTrue
