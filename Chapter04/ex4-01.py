c74f528c6e3290617b9e3c8755a26335cf482f37
repserc-ico <kameshima@std.it-college.ac.jp>
2026-06-02 12:38:20 +0000

    第4章 例題13まで

---
+++ Chapter04/ex4-01.py
@@ -0,0 +1,6 @@
+def f(x):   #関数fの定義の始まり
+    return x * 2    #関数fの終了
+                                    #←関数の区切りの空行
+f(2)    #関数fを呼び出す(呼び出しただけ)
+result = f(5)   #関数fを呼び出した結果を変数resultに代入
+print(result)   #変数resultを表示
---
+++ Chapter04/ex4-13.py
@@ -0,0 +1,5 @@
+def odd_it(x, y=10):    #xは必須引数,yはオプション引数
+    return x + y
+
+result = odd_it(2)
+print(result)
