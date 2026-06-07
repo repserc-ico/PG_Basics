ded119b123e2449e7ff6b0547eb632f051fb4a8b
repserc-ico <kameshima@std.it-college.ac.jp>
2026-06-07 06:34:52 +0000

    第3章 チャレンジ解答例

---
+++ Chapter03/Challenge3-06.py
@@ -0,0 +1,10 @@
+#変数ageの値でいろいろ条件分岐する
+age=12
+if age<=12:
+    print("だいたい小学生以下です")
+elif age>12 and age<=15:
+    print("だいたい中学生です")
+elif age>15 and age<=18:
+    print("だいたい高校生です")
+else:
+    print("社会に出る準備ができています")
