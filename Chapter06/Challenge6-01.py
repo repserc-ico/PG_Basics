cf5aebc96f7ae44383af279410e8af75fe3ce4de
repserc-ico <kameshima@std.it-college.ac.jp>
2026-06-25 06:27:46 +0000

    チャレンジ解答例

--- Chapter06/Challenge6-01.py
+++ Chapter06/Challenge6-01.py
@@ -1,6 +1,6 @@
 #文字列「カミュ」を一文字ずつ表示
 string = "カミュ"
-
-print()
-print()
-print()
+#テキストp89上参照
+print(string[0])
+print(string[1])
+print(string[2])
--- Chapter06/Challenge6-03.py
+++ Chapter06/Challenge6-03.py
@@ -1,4 +1,4 @@
-#文字列の先頭の文字を大文字にするメソッドを使う
+#文字列の先頭の文字を大文字にするメソッドを使う(テキストp92参照)
 string = "aldous Huxley was born in 1894."
 #ただしHuxleyが"huxley"になってもよいものとする
-print()
+print(string.capitalize() )
